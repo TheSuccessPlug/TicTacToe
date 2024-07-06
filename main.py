8204701f83e61d08fad40bdfb988249bbf5054a6
Aaron White <35068256-aaronthastal@users.noreply.replit.com>
2024-07-06 04:50:31 +0000

    first update

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from helpers import print_board
 
 print_board()
+
+#Establish Player Moves
+#Establish Win recognition
+#Establish if spot is taken
+#Establish turn switching between players
+#Establish Tie Game
