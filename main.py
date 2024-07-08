8d74e6b0b4404b31362c06c0dab0dd17fe5ee945
Aaron White <35068256-aaronthastal@users.noreply.replit.com>
2024-07-08 00:18:31 +0000

    secondsave

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from helpers import print_board
 
-print_board()
+spots = {1 : '1', 2 : '2', 3 : '3', 4 : '4', 5 : '5', 6 : '6', 7 : '7', 8 : '8', 9 : '9'}
+
+playing = True
+while playing:
+  print_board(spots)
+  choice = input()
+  if choice == 'q':
+    playing = False
 
 #Establish Player Moves
 #Establish Win recognition
