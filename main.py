50689019732c3c71d2c4f6c87553c02f6825e6b1
Aaron White <35068256-aaronthastal@users.noreply.replit.com>
2024-07-08 20:00:56 +0000

    win

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from helpers import print_board, check_turn
+from helpers import print_board, check_turn, check_win
 import os
 
 spots = {1 : '1', 2 : '2', 3 : '3', 4 : '4', 5 : '5', 6 : '6', 7 : '7', 8 : '8', 9 : '9'}
 
 playing = True
+complete = False
 turn = 0
 while playing:
   os.system('cls' if os.name == 'nt' else 'clear')
@@ -14,27 +15,21 @@ while playing:
     choice = input("Player 2: Which Spot?")
   if choice == 'q':
     playing = False
+  elif str.isdigit(choice) and int(choice) in spots:
+    if not spots[int(choice)] in {"X", "O"}:
+      turn += 1
+      spots[int(choice)] = check_turn(turn)
+    if check_win(spots): playing, complete = False, True 
+    if turn > 8: playing = False
+os.system('cls' if os.name == 'nt' else 'clear')
+print_board(spots)
+
+if complete:
+  if check_turn(turn) == 'X': print("Player 1 Wins!")
+  else: print("Player 2 Wins!")
+else:
+  print("No Winner")
   
-  elif choice == '1':
-    spots[1] = 'X'
-  elif choice == '2' :
-    spots[2] = 'X'
-  elif choice == '3':
-     spots[3] = 'X'
-  elif choice == '4':
-     spots[4] = 'X'
-  elif choice == '5':
-     spots[5] = 'X'
-  elif choice == '6':
-     spots[6] = 'X'
-  elif choice == '7':
-    spots[7] = 'X'
-  elif choice == '8':
-     spots[8] = 'X'
-  elif choice == '9':
-     spots[9] = 'X'
-  turn += 1
-  spots[int(choice)] = check_turn(turn)
   
     
 
