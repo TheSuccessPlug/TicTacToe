89affe3823308fdef4b4231d6b7bb37895e956ac
Aaron White <35068256-aaronthastal@users.noreply.replit.com>
2024-07-08 00:39:17 +0000

    niceedits

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 from helpers import print_board
+import os
 
 spots = {1 : '1', 2 : '2', 3 : '3', 4 : '4', 5 : '5', 6 : '6', 7 : '7', 8 : '8', 9 : '9'}
 
 playing = True
 while playing:
+  os.system('cls' if os.name == 'nt' else 'clear')
   print_board(spots)
-  choice = input()
+  choice = input("Player 1: Which Spot?")
   if choice == 'q':
     playing = False
+  elif choice == '1':
+    spots[1] = 'X'
+  elif choice == '2' :
+    spots[2] = 'X'
+  elif ask_player == '3':
+    spots[3] = 'X'
+  elif ask_player == '4':
+    spots[4] = 'X'
+  elif ask_player == '5':
+    spots[5] = 'X'
+  elif ask_player == '6':
+    spots[6] = 'X'
+  elif ask_player == '7':
+    spots[7] = 'X'
+  elif ask_player == '8':
+    spots[8] = 'X'
+  elif ask_player == '9':
+    spots[9] = 'X'
+  
+    
 
-#Establish Player Moves
+
+
+
+
 #Establish Win recognition
 #Establish if spot is taken
 #Establish turn switching between players
